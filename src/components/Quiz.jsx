3ab7ba81be24c58a75cf8a606f40d3912d7fd9cb
unknown <lukacc008@gmail.com>
2024-06-22 13:53:55 +0200

    Questions are now shuffled every time app loads or refreshes, and the correct answer is not always the 1st question

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -6,6 +6,8 @@ export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
 
   const activeQuestionIndex = userAnswers.length;
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
 
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prevUserAnswers) => {
@@ -13,14 +15,14 @@ export default function Quiz() {
     });
   }
 
-  console.log('User answers:', userAnswers);
+  console.log("User answers:", userAnswers);
 
   return (
     <div id="quiz">
       <div id="question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          {shuffledAnswers.map((answer) => (
             <li key={answer} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
