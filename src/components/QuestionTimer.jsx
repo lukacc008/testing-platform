aa3d239fd0c3a34752f238e37082b573b7a87849
unknown <lukacc008@gmail.com>
2024-06-27 17:32:28 +0200

    Added KEY to component that I want to rerender when the timer runs out, and added cleanup functions for setTimeout and setInterval

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -4,16 +4,24 @@ export default function QuestionTimer({ timeout, onTimeout }) {
   const [remainingTime, setRemainingTime] = useState(timeout);
 
   useEffect(() => {
-    console.log('Setting timeout');
-    setTimeout(onTimeout, timeout);
+    console.log("Setting timeout");
+    const timer = setTimeout(onTimeout, timeout);
+
+    return () => {
+      clearTimeout(timer);
+    };
   }, [timeout, onTimeout]);
 
   useEffect(() => {
-    console.log('Setting interval');
-    setInterval(() => {
+    console.log("Setting interval");
+    const interval = setInterval(() => {
       setRemainingTime((prevRemainingTime) => prevRemainingTime - 100);
     }, 100);
+
+    return () => {
+      clearInterval(interval);
+    };
   }, []);
 
-  return <progress id="question-time" max={timeout} value={remainingTime}/>;
+  return <progress id="question-time" max={timeout} value={remainingTime} />;
 }
