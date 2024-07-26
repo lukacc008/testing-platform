2d0f034791a9e023a1311cb5cecd633c7c3eeb7a
unknown <lukacc008@gmail.com>
2024-07-26 17:29:35 +0200

    Started working on registration form.

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React from "react";
+import Register from "./Register";
 
 function StartScreen({ onStart }) {
   return (
-    <div id="start">
+    <>
+      <div id="start">
         <p>By pressing the 'I AM READY' button your test will begin.</p>
-        <p style={{ color: 'red' }}>Any unanswered questions will be marked as wrong answers.</p>
-      <button onClick={onStart}>I AM READY</button>
-    </div>
+        <p style={{ color: "red" }}>
+          Any unanswered questions will be marked as skipped which is basically wrong.
+        </p>
+        <button onClick={onStart}>I AM READY</button>
+      </div>
+      <Register />
+    </>
   );
 }
 
