7958433c38a8a9f2edd41fe1ff05b047aec47488
unknown <lukacc008@gmail.com>
2024-07-30 18:19:43 +0200

    Working on node.js and backend for the app

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const logEvents = require('./logEvents');
+
+const EventEmitter = require('events');
+
+class MyEmitter extends EventEmitter {};
+
+// initialize object
+const myEmitter = new MyEmitter();
+
+// add listener for the log event
+myEmitter.on('log', (msg) => logEvents(msg));
+
