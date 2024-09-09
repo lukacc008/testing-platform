8d755712645e00e33658690c5c77d3afd073c3ef
unknown <lukacc008@gmail.com>
2024-09-09 16:37:32 +0200

    Role based authorization works for results. It still does not work as I want.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,14 @@ import { Route, Routes } from "react-router-dom"; // Removed BrowserRouter
 import Login from "./components/Login";
 import Register from "./components/Register";
 import UserTable from "./components/UserTable";
+import Unauthorized from "./components/Unauthorized";
+import RequireAuth from "./components/RequireAuth";
+
+const ROLES = {
+  User: 2001,
+  Editor: 1984,
+  Admin: 5150,
+};
 
 function App() {
   return (
@@ -12,9 +20,12 @@ function App() {
       <main>
         <Routes>
           <Route path="*" element={<Quiz />} />
+          <Route path="unauthorized" element={<Unauthorized />} />
           <Route path="register" element={<Register />} />
           <Route path="login" element={<Login />} />
-          <Route path="results" element={<UserTable />} />
+          <Route element={<RequireAuth allowedRoles={[ROLES.Admin]} />}>
+            <Route path="results" element={<UserTable />} />
+          </Route>
           {/* other routes */}
         </Routes>
       </main>
