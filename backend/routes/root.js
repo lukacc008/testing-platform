ecff15b2042a28cc07361c3cf1f0a2a8e71b7617
unknown <lukacc008@gmail.com>
2024-12-25 00:57:48 +0100

    Fixing

--- backend/routes/root.js
+++ backend/routes/root.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
+// Serve the quiz-logo.png image from the 'public' directory
 router.get('^/$|/index(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
+    res.sendFile(path.join(__dirname, '..', 'public', 'quiz-logo.png'));
 });
 
 module.exports = router;
