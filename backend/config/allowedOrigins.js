805734aedbfe897aa9fda21ef822f574c99081ef
unknown <lukacc008@gmail.com>
2024-12-24 23:37:08 +0100

    Trying to host the app with Vercel for frontend and Render for backend

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -3,7 +3,8 @@ const allowedOrigins = [
     'http://localhost:5173',
     'http://127.0.0.1:5500',
     'http://localhost:3500',
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'https://testing-platform-woad.vercel.app/home'
 ];
 
 module.exports = allowedOrigins;
