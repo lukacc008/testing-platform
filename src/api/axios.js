1fb5bd03c35b1405488c2ae2c5dbca75b32485bd
unknown <lukacc008@gmail.com>
2024-12-25 16:55:40 +0100

    Fixing 3

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,13 @@
 import axios from "axios";
 
+// Get the base URL dynamically based on the environment
+const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:3500';
+
 export default axios.create({
-    baseURL: 'http://localhost:3500'
+    baseURL: BASE_URL,
 });
 
 export const axiosPrivate = axios.create({
-    baseURL: "http://localhost:3500",
+    baseURL: BASE_URL,
     withCredentials: true,
-  });
+});
