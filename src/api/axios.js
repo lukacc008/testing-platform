7a11530db8a0c3c4c4a06fba25f682e69032281f
unknown <lukacc008@gmail.com>
2024-12-28 15:01:35 +0100

    Small fix

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,4 +1,3 @@
-
 //TESTING FOR LOCAL DEVELOPMENT
 // import axios from "axios";
 
@@ -11,31 +10,29 @@
 //     withCredentials: true,
 //   });
 
+//TESTING FOR PRODUCTION WITHOUT HARDCODING BASE_URL
+// import axios from "axios";
+// const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:3500';
+// console.log('Base URL:', BASE_URL); // Debugging
+// export default axios.create({
+//     baseURL: BASE_URL,
+// });
 
-  //TESTING FOR PRODUCTION WITHOUT HARDCODING BASE_URL
-  // import axios from "axios";
-  // const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:3500';
-  // console.log('Base URL:', BASE_URL); // Debugging
-  // export default axios.create({
-  //     baseURL: BASE_URL,
-  // });
-  
-  // export const axiosPrivate = axios.create({
-  //     baseURL: BASE_URL,
-  //     withCredentials: true,
-  // });
+// export const axiosPrivate = axios.create({
+//     baseURL: BASE_URL,
+//     withCredentials: true,
+// });
 
-  // TESTING FOR PRODUCTION WITH HARDCODED BASE_URL
-  const BASE_URL = 'https://testing-platform-xzzf.onrender.com'; // Hardcoded backend URL
-console.log('Base URL:', BASE_URL);
+// TESTING FOR PRODUCTION WITH HARDCODED BASE_URL
+import axios from "axios";
+const BASE_URL = "https://testing-platform-xzzf.onrender.com"; // Hardcoded backend URL
+console.log("Base URL:", BASE_URL);
 
 export default axios.create({
-    baseURL: BASE_URL,
+  baseURL: BASE_URL,
 });
 
 export const axiosPrivate = axios.create({
-    baseURL: BASE_URL,
-    withCredentials: true,
+  baseURL: BASE_URL,
+  withCredentials: true,
 });
-
-  
