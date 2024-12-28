7f9512a257776c5dd2a72bc4a6fc48d83f9ca9d5
unknown <lukacc008@gmail.com>
2024-12-28 15:11:31 +0100

    Changed allowed origins to accept some endpoints

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -6,7 +6,9 @@ const allowedOrigins = [
     'http://localhost:3000',
     'https://testing-platform-woad.vercel.app',
     'https://testing-platform-woad.vercel.app/home',
-    'https://testing-platform-xzzf.onrender.com'
+    'https://testing-platform-xzzf.onrender.com',
+    'https://testing-platform-8enfntx2q-lukas-projects-263d9842.vercel.app', // Current Vercel frontend
+    'https://testing-platform-xzzf.onrender.com' // Backend URL
 ];
 
 module.exports = allowedOrigins;
