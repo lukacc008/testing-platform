debb30ba6bdfb8a8fd8f91c9c43384796befa69c
unknown <lukacc008@gmail.com>
2024-12-29 16:07:29 +0100

    Added a note for people to know why requests at first are taking long

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React from "react";
 
 const Home = () => {
   return (
     <div>
       <h1>Welcome to the Candidate Testing Platform</h1>
+      <p style={{ color: "red" }}>
+        NOTE: FIRST REQUESTS MAY TAKE UP TO 40+ SECONDS DUE TO USING FREE
+        VERSION OF RENDER
+      </p>
       <p>
-        This application is designed to help our company efficiently test and evaluate candidates 
-        for open positions. Through our online testing platform, candidates can complete a series 
-        of tests as part of the initial screening process. This allows us to identify and filter 
-        top candidates before moving forward in the hiring process.
+        This application is designed to help our company efficiently test and
+        evaluate candidates for open positions. Through our online testing
+        platform, candidates can complete a series of tests as part of the
+        initial screening process. This allows us to identify and filter top
+        candidates before moving forward in the hiring process.
       </p>
     </div>
   );
