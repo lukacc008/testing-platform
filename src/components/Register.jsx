ec6cdfbe32bdfc50fecc28badf2cd5fdc5661ee9
unknown <lukacc008@gmail.com>
2025-01-19 16:19:46 +0100

    Fixed the confirm password note

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -246,27 +246,28 @@ const Register = () => {
                 className={validMatch || !matchPwd ? "hide" : "invalid"}
               />
             </label>
-            <input
-              type="password"
-              id="confirm_pwd"
-              onChange={(e) => setMatchPwd(e.target.value)}
-              value={matchPwd}
-              required
-              aria-invalid={validMatch ? "false" : "true"}
-              aria-describedby="confirmnote"
-              onFocus={() => setMatchFocus(true)}
-              onBlur={() => setMatchFocus(false)}
-            />
-            <p
-              id="confirmnote"
-              className={
-                matchFocus && !validMatch ? "instructions" : "offscreen"
-              }
-            >
-              <FontAwesomeIcon icon={faInfoCircle} />
-              Must match the first password input field.
-            </p>
-
+            <div style={{ position: "relative", display: "inline-block" }}>
+              <input
+                type="password"
+                id="confirm_pwd"
+                onChange={(e) => setMatchPwd(e.target.value)}
+                value={matchPwd}
+                required
+                aria-invalid={validMatch ? "false" : "true"}
+                aria-describedby="confirmnote"
+                onFocus={() => setMatchFocus(true)}
+                onBlur={() => setMatchFocus(false)}
+              />
+              <p
+                id="confirmnote"
+                className={
+                  matchFocus && !validMatch ? "instructions" : "offscreen"
+                }
+              >
+                <FontAwesomeIcon icon={faInfoCircle} />
+                Must match the first password input field.
+              </p>
+            </div>
             <button
               disabled={
                 !validName || !validPwd || !validMatch || !validEmail || loading
