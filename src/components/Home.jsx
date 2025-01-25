4f0d3b686028220f721114df433cc664a831118b
unknown <lukacc008@gmail.com>
2025-01-25 14:16:14 +0100

    Migrating to Tailwind and changing few things

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,20 +2,20 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="flex flex-col justify-center items-center h-[35vh] text-center">
+    <div className="flex flex-col justify-center items-center h-[30vh] text-center">
       <h1 className="text-xl font-bold">
         Welcome to the Candidate Testing Platform
       </h1>
-      <p className="text-red-500">
-        NOTE: FIRST REQUEST MAY TAKE UP TO 40+ SECONDS DUE TO USING FREE
-        VERSION OF RENDER
-      </p>
       <p className="mt-4">
         This application is designed to help our company efficiently test and
         evaluate candidates for open positions. Through our online testing
         platform, candidates can complete a series of tests as part of the
         initial screening process. This allows us to identify and filter top
         candidates before moving forward in the hiring process.
+      </p>
+      <p className="text-red-500 mt-5">
+        NOTE: FIRST REQUEST MAY TAKE UP TO 40+ SECONDS DUE TO USING FREE
+        VERSION OF RENDER
       </p>
     </div>
   );
